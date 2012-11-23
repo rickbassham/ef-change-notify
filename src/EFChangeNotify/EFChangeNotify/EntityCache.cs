6fee739aa19e9cbc8dddde704a546180a20ee26f
rickbassham <brodrick.bassham@gmail.com>
2012-11-23 10:03:00 -0600

    Add caching support for entity queries.

---
+++ src/EFChangeNotify/EFChangeNotify/EntityCache.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Caching;
+
+namespace EFChangeNotify
+{
+    public class EntityCache<TEntity, TDbContext>
+        : IDisposable
+        where TDbContext : DbContext, new()
+        where TEntity : class
+    {
+        private DbContext _context;
+        private Expression<Func<TEntity, bool>> _query;
+
+        private string _cacheKey = Guid.NewGuid().ToString();
+
+        public EntityCache(Expression<Func<TEntity, bool>> query)
+        {
+            _context = new TDbContext();
+            _query = query;
+        }
+
+        private IEnumerable<TEntity> GetCurrent()
+        {
+            var query = _context.Set<TEntity>().Where(_query);
+
+            return query;
+        }
+
+        private IEnumerable<TEntity> GetResults()
+        {
+            List<TEntity> value = MemoryCache.Default[_cacheKey] as List<TEntity>;
+
+            if (value == null)
+            {
+                value = GetCurrent().ToList();
+
+                var changeMonitor = new EntityChangeMonitor<TEntity, TDbContext>(_query);
+
+                CacheItemPolicy policy = new CacheItemPolicy();
+
+                policy.ChangeMonitors.Add(changeMonitor);
+
+                MemoryCache.Default.Add(_cacheKey, value, policy);
+
+                Console.WriteLine("From Database...");
+            }
+            else
+            {
+                Console.WriteLine("From Cache...");
+            }
+
+            return value;
+        }
+
+        public IEnumerable<TEntity> Results
+        {
+            get
+            {
+                return GetResults();
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_context != null)
+                {
+                    _context.Dispose();
+                    _context = null;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ src/EFChangeNotify/EFChangeNotify/EntityChangeMonitor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using System.Runtime.Caching;
+
+namespace EFChangeNotify
+{
+    public class EntityChangeMonitor<TEntity, TDbContext> : ChangeMonitor,
+        IDisposable
+        where TDbContext : DbContext, new()
+        where TEntity : class
+    {
+        private DbContext _context;
+        private Expression<Func<TEntity, bool>> _query;
+        private EntityChangeNotifier<TEntity, TDbContext> _changeNotifier;
+
+        private string _uniqueId;
+
+        public EntityChangeMonitor(Expression<Func<TEntity, bool>> query)
+        {
+            _context = new TDbContext();
+            _query = query;
+            _uniqueId = Guid.NewGuid().ToString();
+            _changeNotifier = new EntityChangeNotifier<TEntity, TDbContext>(_query);
+
+            _changeNotifier.Error += new EventHandler<NotifierErrorEventArgs>(_changeNotifier_Error);
+            _changeNotifier.Changed += new EventHandler<EntityChangeEventArgs<TEntity>>(_changeNotifier_Changed);
+
+            InitializationComplete();
+        }
+
+        void _changeNotifier_Error(object sender, NotifierErrorEventArgs e)
+        {
+            base.OnChanged(null);
+        }
+
+        void _changeNotifier_Changed(object sender, EntityChangeEventArgs<TEntity> e)
+        {
+            base.OnChanged(e.Results);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_changeNotifier != null)
+                {
+                    _changeNotifier.Dispose();
+                    _changeNotifier = null;
+                }
+            }
+        }
+
+        public override string UniqueId
+        {
+            get { return _uniqueId; }
+        }
+    }
+}
